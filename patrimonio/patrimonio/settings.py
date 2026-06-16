678f5fc311dbb9bc2efd57d65e35bc0d8bf387a4
leosaq <leandrosaquisari7@gmail.com>
2026-06-16 14:09:02 -0500

    CAMBIOS3

--- patrimonio/patrimonio/settings.py
+++ patrimonio/patrimonio/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'patrimonio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'patrimonio_db',
+        'USER': 'patrimonio_user',
+        'PASSWORD': 'patrimonio123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
